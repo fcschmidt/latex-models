dd39d13c869804ef559ce39fa612332e54c95511
fcschmidt <fcschimidt@gmail.com>
2017-06-19 15:39:48 -0300

    Uptade Compiler

--- aps-model/compiler.sh
+++ aps-model/compiler.sh
@@ -4,11 +4,21 @@
 # Gera o PDF e exclui arquivos inúteis
 
 pdflatex aps.tex
+
 bibtex aps.aux
+
+makeindex aps.idx
+
 pdflatex aps.tex
+
+pdflatex aps.tex
+
 rm -f *.log *.lof *.idx *.aux *.toc *.out *.dvi *.ps *.lot *.bbl *.ilg *.ind *.blg;
+
 cd arquivos
+
 rm -f *.log *.lof *.idx *.aux *.toc *.out *.dvi *.ps *.lot *.bbl *.ilg *.ind *.blg;
+
 
 
 
