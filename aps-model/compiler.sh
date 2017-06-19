640912cb85961d3653de6592667781f038dbc896
fcschmidt <fcschimidt@gmail.com>
2017-06-19 11:09:24 -0300

    Updade: Clean Version

--- aps-model/compiler.sh
+++ aps-model/compiler.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Compiler
-# Compila e exclui arquivos inúteis
+# Compilador
+# Gera o PDF e exclui arquivos inúteis
 
 pdflatex aps.tex
-
 bibtex aps.aux
-
 pdflatex aps.tex
-
 rm -f *.log *.lof *.idx *.aux *.toc *.out *.dvi *.ps *.lot *.bbl *.ilg *.ind *.blg;
-
 cd arquivos
-
 rm -f *.log *.lof *.idx *.aux *.toc *.out *.dvi *.ps *.lot *.bbl *.ilg *.ind *.blg;
 
 
